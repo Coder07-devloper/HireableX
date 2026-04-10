45169dc955d22469580d769b8139622a30f54fab
Coder07 <siyasri19.14@123>
2026-04-11 03:45:17 +0530

    Added root route

--- Backend/server.js
+++ Backend/server.js
@@ -9,3 +9,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(3000, () => {
     console.log(`Server is running on port ${PORT}`)
 })
+
+app.get("/", (req, res) => {
+  res.send("HirableX Backend is Running 🚀");
+});
